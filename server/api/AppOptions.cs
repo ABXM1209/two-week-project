3bc4812a93ab0763c47b642fb29b3e6056f8f8fb
Abdurahman Haji Abdi <abdulrahmanxambali@gmail.com>
2025-09-16 17:50:06 +0200

    deployment

---
+++ server/api/AppOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public class AppOptions
+{
+    [MinLength(1)]
+    public string ConnectionString { get; set; }
+    [MinLength(1)]
+    public string JwtSecret { get; set; }
+}
--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,20 +1,25 @@
+using api;
 using efscaffold.Entities;
 using Infrastructure.Postgres.Scaffolding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
 {
-    DotNetEnv.Env.Load(".env");
-    var connStr = Environment.GetEnvironmentVariable("CONN_STR");
-    conf.UseNpgsql(connStr);
+    conf.UseNpgsql(appOptions.ConnectionString);
 });
 
 var app = builder.Build();
 
-app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+app.MapGet("/", (
+    
+    [FromServices]IOptionsMonitor<AppOptions> optionsMonitor,
+    [FromServices]MyDbContext dbContext) =>
 {
     var myAuthor = new Author()
     {
